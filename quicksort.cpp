0758ab687d3fb73cfae10fc0904758354aee72ab
Majid Tamboli <169020141+majidtamboli45@users.noreply.github.com>
2026-02-28 16:52:42 +0530

    Add files via upload

---
+++ quicksort.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int partition(vector<int> &arr, int low, int high) {
+    int pivot = arr[low];
+    int i = low;
+    int j = high;
+
+    while (i < j) {
+        while (arr[i] <= pivot && i <= high - 1) {
+            i++;
+        }
+
+        while (arr[j] > pivot && j >= low + 1) {
+            j--;
+        }
+
+        if (i < j)
+            swap(arr[i], arr[j]);
+    }
+
+    swap(arr[low], arr[j]);
+    return j;   // correct pivot position
+}
+
+void qs(vector<int> &arr, int low, int high) {
+    if (low < high) {
+        int pIndex = partition(arr, low, high);
+        qs(arr, low, pIndex - 1);
+        qs(arr, pIndex + 1, high);
+    }
+}
+
+int main() {
+    vector<int> arr = {10, 7, 8, 9, 1, 5};
+
+    cout << "Original array: ";
+    for (int x : arr)
+        cout << x << " ";
+
+    qs(arr, 0, arr.size() - 1);
+
+    cout << "\nSorted array: ";
+    for (int x : arr)
+        cout << x << " ";
+
+    return 0;
+}
